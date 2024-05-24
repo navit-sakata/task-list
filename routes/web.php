97d34e7306b279595a451dac0c07f7a930fa4c07
sakata.shun2 <sakata@navit-j.com>
2024-05-24 09:34:52 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TasksController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +11,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TasksController::class, 'index']);
+Route::resource('task', TasksController::class);
+// CRUD
+// // メッセージの個別詳細ページ表示
+// Route::get('task/{id}', [TasksController::class, 'show']);
+// // メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+// Route::post('task', [TasksController::class, 'store']);
+// // メッセージの更新処理（編集画面を表示するためのものではありません）
+// Route::put('task/{id}', [TasksController::class, 'update']);
+// // メッセージを削除
+// Route::delete('task/{id}', [TasksController::class, 'destroy']);
+// // index: showの補助ページ
+// Route::get('task', [TasksController::class, 'index'])->name('task.index');
+// // create: 新規作成用のフォームページ
+// Route::get('task/create', [TasksController::class, 'create'])->name('task.create');
+// // edit: 更新用のフォームページ
+// Route::get('task/{id}/edit', [TasksController::class, 'edit'])->name('task.edit');
